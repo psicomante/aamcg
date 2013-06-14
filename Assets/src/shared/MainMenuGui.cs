875d205bb962b3153db09a4f2d91ce54c6cd7480
Chosko <ruben.caliandro@gmail.com>
2013-06-14 12:57:35 +0200

    code reorganization

---
+++ Assets/src/shared/MainMenuGui.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Main menu GUI.
+/// </summary>
+public class MainMenuGui : MonoBehaviour {
+
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	void Start () {
+	
+	}
+	
+	/// <summary>
+	/// Update this instance.
+	/// </summary>
+	void Update () {
+	
+	}
+}
