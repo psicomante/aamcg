fbee5be9861542e9a7840e7f335b2cd3ac96d660
Psicomante <psicomante@gmail.com>
2013-06-16 12:59:03 +0200

    Server GUI - Connections and IPAddress
    Should Fix #9 #10

--- Assets/src/shared/GameGui.cs
+++ Assets/src/shared/GameGui.cs
@@ -6,25 +6,33 @@ using Amucuga;
 /// GUI for server in the Game Scene
 /// </summary>
 public class GameGui : MonoBehaviour {
+	
+	private string _serverIPAddress;
 
 	/// <summary>
 	/// Start the Game GUI
 	/// </summary>
-	void Start () {
-	    //Blocks client execution
-        if (Network.isClient)
-            return;
+	void Start ()
+	{
+		//Blocks client execution
+		if (Network.isClient)
+			return;
+		
+		_serverIPAddress = NetworkManager.GetLocalIPAddress();
+		
 	}
 
     /// <summary>
     /// Renders the GUI
     /// </summary>
-    void OnGUI()
-    {
-        if (GUI.Button(new Rect(10, 60, 180, 25), "Logout"))
-        {
-            Network.Disconnect(500);
-            AmApplication.LoadMainMenu ();
-        }
-    }
+    void OnGUI ()
+	{
+		GUILayout.Label ("IP Address: " + _serverIPAddress);
+		GUILayout.Label ("Connections: " + Network.connections.Length);
+		
+		if (GUI.Button (new Rect (10, 60, 180, 25), "Logout")) {
+			NetworkManager.Shutdown ();
+			AmApplication.LoadMainMenu ();
+		}		
+	}
 }
--- Assets/src/shared/NetworkManager.cs
+++ Assets/src/shared/NetworkManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Net;
 using Amucuga;
 
 public class NetworkManager : MonoBehaviour {
@@ -12,6 +13,7 @@ public class NetworkManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
+		//TODO: maybe move this code to OnDisconnect event.
 		if (Network.peerType == NetworkPeerType.Disconnected) 
 			AmApplication.LoadMainMenu ();
 	
@@ -34,8 +36,34 @@ public class NetworkManager : MonoBehaviour {
 	/// <summary>
 	/// Logout from server (client code?)
 	/// </summary>
-	public static void Logout (){
+	public static void Logout ()
+	{
 		
+	
 	}
+	
+	/// <summary>
+	/// Gets the server local IP Address.
+	/// TODO: to get the external ip address you must query an external service (maybe the MasterServer)
+	/// </summary>
+	/// <returns>
+	/// The local IP Address
+	/// </returns>
+	public static string GetLocalIPAddress ()
+	{
+		string HostName = Dns.GetHostName ();
+		print (HostName);
+		
+		IPAddress[] ipAddresses = System.Net.Dns.GetHostAddresses (HostName);
+		return ipAddresses[0].ToString();
+		/*
+		IPHostEntry ipEntry = System.Net.Dns.GetHostEntry (HostName);
+		
+		IPAddress[] ipAddressesEntry = ipEntry.AddressList;
+		for (int i = 0; i < ipAddressesEntry.Length; i++)
+			print (ipAddressesEntry [i].ToString ());			
+		return HostName;*/		
+	}
+	
 	
 }
